32105cebb4660f6d13d3b5c2327c3b774b977d87
Dhanshrisiddh <151831423+Dhanshrisiddh@users.noreply.github.com>
2024-08-17 21:35:59 +0530

    Bank Details
    Bank Details

---
+++ 03-funAssign01.js
@@ -0,0 +1,46 @@
+
+console.log("============== 1.no argument and no return type===================");
+
+
+function show()
+{
+    console.log("Good Morning !");
+}
+show();
+
+
+function display()
+{
+    console.log("I am T.Y.B.Tech CSE Student");
+} 
+display();
+
+console.log("===========2.with argument and no return type==============");
+
+function personalDetails(firstName,lastName,collageName)
+{
+    console.log("My First Name is :",firstName);
+    console.log("My Last Name :",lastName);
+    console.log("My Collage Name is :",collageName);
+    
+}
+personalDetails("Dhanshri","Siddh","R.I.T,Islampur");
+
+console.log("==========with argument and no return type==============");
+
+function addThreeValues(firstNo,secondNo,thirdNo)
+{
+    console.log("First No is :",firstNo);
+    console.log("Second No is :",secondNo);
+    console.log("Third No is :",thirdNo);
+     var result=firstNo+secondNo+thirdNo;
+    console.log("Addition is :",result);
+    
+}
+addThreeValues(10.23,600,40);
+addThreeValues("Hello","Good","Morning");
+
+
+
+
+
