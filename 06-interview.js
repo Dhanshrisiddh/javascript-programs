86b9c2c6850da249585213565e4a2095b05a64dd
Dhanshrisiddh <151831423+Dhanshrisiddh@users.noreply.github.com>
2024-08-22 23:04:51 +0530

    If-else Statement
    If-else Statement

---
+++ 06-interview.js
@@ -0,0 +1,14 @@
+
+
+console.log(`========== if else statement- Interview Eligible ==========`);
+
+var interview =function student(gradScore,hscScore,sscScore,candidateName){
+    if (gradScore>=70 || hscScore>=80 || sscScore>90) {
+        console.log(`Congrates ${candidateName} you are eligible for TCS interview.`);
+    } else {
+        console.log(`Unfortunately you are not eligible for TCS interview.`);
+    }
+}
+interview(80,86,90,"Dhanshri");
+interview(70,65,55,"Rutuja");
+interview(60,79,88,"Om");
