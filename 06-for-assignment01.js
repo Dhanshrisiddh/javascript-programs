4703ddc9616fc23e5c797e359ccb8980254c5903
Dhanshrisiddh <151831423+Dhanshrisiddh@users.noreply.github.com>
2024-08-23 20:13:44 +0530

    06-for-assignment01
    06-for-assignment01

---
+++ 06-for-assignment01.js
@@ -0,0 +1,55 @@
+
+//WAP to print number from 5 to 15 by incrementing 1
+console.log("==================print number from 5 to 15 by incrementing 1=======================");
+
+for (let index = 5; index <= 15; index++) {
+    console.log(index);
+}
+
+//WAP tp print number from 50 to 40 by decrementing 1
+console.log("=====================WAP tp print number from 50 to 40 by decrementing 1====================");
+
+for (let index = 50; index >= 40; index--) {
+    console.log(index);
+}
+
+//WAP to find first 15 odd numbers
+console.log("=========================find first 15 odd numbers=======================");
+
+for (let index = 1; index <= 30 ; index=index+2) {
+    console.log(index);
+}
+
+//WAP to find first 10 even numbers
+console.log("==================find first 10 even numbers===================");
+
+for (let index = 2; index <= 20; index=index+2) {
+    console.log(index);
+}
+
+//WAP  to print table 5 like 5,10,15,20,25...50 
+console.log("============== print table 5 like 5,10,15,20,25...50 =====================");
+
+for (let index = 5; index <= 50 ; index=index+5) {
+    console.log(index);
+}
+
+//WAP to print table of 10 like 10 20 30 40..100
+console.log("==============print table of 10 like 10 20 30 40.......100==================");
+
+for (let index = 10; index <=100 ; index=index+10) {
+    console.log(index);
+}
+
+// WAP to print table of 10 in reverse order like 100 90 80 ..10
+console.log("==================Print table of 10 in reverse order like 100 90 80 ....10================")
+
+for (let index = 100; index >=10 ; index=index-10) {
+    console.log(index);
+}
+
+
+
+
+
+
