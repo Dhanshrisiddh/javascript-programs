29144295cc01772ca3be3abdd9facbb0135fd011
Dhanshrisiddh <151831423+Dhanshrisiddh@users.noreply.github.com>
2024-08-27 23:07:29 +0530

    operator.comparision
    operator.comparision

---
+++ 04-operator.comparision.js
@@ -0,0 +1,30 @@
+
+
+var n1 ="50";
+var n2 = 50;
+var result = n1==n2;
+
+// double equal
+console.log(n1==n2)  // It check only values 
+
+// triple equal
+console.log(n1===n2); // It checks values and data types as well(Strictl Check)
+
+
+var n1 = 10;
+var n2 = 20;
+
+//GREATER THAN
+console.log(n1 != n2);
+console.log(n1>n2);
+console.log(n1>10);
+console.log(n1>=10);
+
+//LESS THAN
+console.log(n1<n2);
+console.log(n1<10);
+console.log(n1<=10);
+
+
+
+
