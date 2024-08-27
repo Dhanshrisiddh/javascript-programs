d5dbac376552a9ce1db056562990b1f967d86bfc
Dhanshrisiddh <151831423+Dhanshrisiddh@users.noreply.github.com>
2024-08-27 23:11:27 +0530

    if-statement
    if-statement

---
+++ 06-if-statement.js
@@ -0,0 +1,13 @@
+
+// If Condition
+console.log(`Before if block`);
+var age=19;
+if (age>=18) {
+    console.log(`Inside if block`);
+    console.log(`You are eligible for vote :${age}`);
+    
+}
+console.log(`After if block`);
+
+
+
