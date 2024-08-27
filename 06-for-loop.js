846f52156c160f0f211e4f64f986375f412bfa18
Dhanshrisiddh <151831423+Dhanshrisiddh@users.noreply.github.com>
2024-08-27 23:09:47 +0530

    for-loop
    for-loop

---
+++ 06-for-loop.js
@@ -0,0 +1,34 @@
+
+console.log("===========Print the Number from 1 to 10==============");
+for (var i = 0; i <= 10; i++) {
+    console.log(i);
+}
+
+
+console.log("============Print Even Number from 0 to 10=============");
+for (let index = 0; index < 10 ; index=index+2) {
+    console.log(index);
+}
+
+
+console.log("==============Print the Table of 5================");
+for (let index = 5; index <= 50; index=index+5) {
+    console.log(index);
+}
+
+
+console.log("===========Print the Number from 50 to 40==============");
+for (let index = 50; index >= 40; index--) {
+    console.log(index);
+}
+
+
+console.log("==============Print the Table of 9 in reverse order================");
+for (let index = 90; index >=9 ; index=index-9) {
+    console.log(index);
+}
+
+console.log("===============Print the Table of 5 in reverse order====================");
+for (let  index = 50; index >= 5; index=index-5) {
+    console.log(index);
+}
