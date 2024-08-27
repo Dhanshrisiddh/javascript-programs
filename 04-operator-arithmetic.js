12ec002b7a1dcaa3c0849cdceb3fcd051e458f38
Dhanshrisiddh <151831423+Dhanshrisiddh@users.noreply.github.com>
2024-08-27 23:08:12 +0530

    operator-arithmetic
    operator-arithmetic

---
+++ 04-operator-arithmetic.js
@@ -0,0 +1,29 @@
+console.log(`================Arithmetic Operattors=================`);
+var n1=10;
+var n2=5;
+
+var result=n1+n2;
+console.log(`Addition is :${result}`);
+
+var result=n1- n2;
+console.log(`Substraction is: ${result}`);
+
+var result=n1*n2;
+console.log(`Multiplication is: ${result}`);
+
+var result=n1/n2;
+console.log(`Division is: ${result}`);    // It return Division
+    
+var result=n1 % n1;
+console.log(`Modules is: ${result}`);     // It Return Reminder
+
+var result=n1 % 3;
+console.log(`Modules is: ${result}`);   
+
+var result = n1 ** 2;
+console.log(`Exponential is: ${result}`); 
+
+var result = n1 ** n2;
+console.log(`Exponential is: ${result}`); 
+
+
